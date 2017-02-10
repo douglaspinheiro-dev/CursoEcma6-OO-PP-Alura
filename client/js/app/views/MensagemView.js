e180dfa7097c98341147107b4134e993f5391195
dougkriok <douglas.pinheiro@icloud.com>
2017-02-09 23:32:19 -0200

    Limpeza de código em MensagemView

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -1,7 +1,7 @@
 class MensagemView extends View {
+	
 	constructor(elemento){
 		super(elemento);
-		this._id = '#mensagemView';
 		this._url = 'js/app/views/templates/alert_MensagemView.html';
 		
 	}
